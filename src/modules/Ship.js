a56c1e928992a5ae5f82bf515c09364d9f70b860
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-08-05 22:51:38 +0500

    Add isHIt function to check if a coordinate is hit

--- src/modules/Ship.js
+++ src/modules/Ship.js
@@ -8,7 +8,17 @@ const Ship = function (id, coords) {
       this.hit.push(coord);
     },
     isSunk: function () {
-      return (this.hit.length === this.coords.length) ? true : false;
+      return this.hit.length === this.coords.length ? true : false;
+    },
+    isHit: function (x, y) {
+      return this.hits.forEach((coords) => {
+        coords.forEach((coord) => {
+          if (coord[0] === x && coord[1] === y) {
+            return true;
+          }
+        });
+        return false;
+      });
     },
   };
 };
