276d9bd39058f39a0ac0be7f5a381b3de6a27fe2
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-08-05 23:36:30 +0500

    Convert isHit to generic  contains function

--- src/modules/Ship.js
+++ src/modules/Ship.js
@@ -10,15 +10,15 @@ const Ship = function (id, coords) {
     isSunk: function () {
       return this.hit.length === this.coords.length ? true : false;
     },
-    isHit: function (x, y) {
-      return this.hits.forEach((coords) => {
-        coords.forEach((coord) => {
-          if (coord[0] === x && coord[1] === y) {
-            return true;
-          }
-        });
-        return false;
+    contains: function (arr, obj) {
+      let compare = false
+      this[obj].forEach((coord) => {
+        if (coord[0] === arr[0] && coord[1] === arr[1]) {
+          compare = true;
+          return;
+        }
       });
+      return compare
     },
   };
 };
--- src/tests/Ship.test.js
+++ src/tests/Ship.test.js
@@ -6,10 +6,9 @@ test("Adds coordinates to hit", () => {
     [0, 2],
     [0, 3],
   ]);
-
+  
   ship.processHit([0, 1]);
-  ship.isHit(0, 1);
-  expect(ship.isHit(0, 1)).toBe(true);
+  expect(ship.contains([0, 1], "hit")).toBe(true);
 });
 
 test("Reports if ship is sunk", () => {
