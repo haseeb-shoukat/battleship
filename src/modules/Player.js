530c57622deca60cf4a63a42297ae8a02bbaa3c7
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-08-06 17:29:48 +0500

    Create Player class

---
+++ src/modules/Player.js
@@ -0,0 +1,29 @@
+import { GameBoard } from "./GameBoard"
+
+class Player {
+    constructor(id = "player") {
+        this.myBoard = GameBoard();
+        this.enemy;
+        this.id = id;
+    }
+
+    set enemyBoard(player) {
+        this.enemy = player
+    }
+
+    attack(x, y) {
+        this.enemy.receive(x, y)
+    }
+
+    compAttack() {
+        if (this.id != "comp") return;
+        let legalMoves = this.enemy.myBoard.legalMoves();
+        let [x, y] = legalMoves[Math.random(legalMoves.length)];
+        this.enemy.receive(x, y)
+    }
+
+    receive(x, y) {
+        if (this.myBoard.isIllegal(x, y)) return "Illegal Move";
+        this.myBoard.receiveAttack(x, y);
+    }
+}
