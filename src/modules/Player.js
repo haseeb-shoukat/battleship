81456eeaccc806c2a62287851d88720661e9d930
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-08-09 03:37:46 +0500

    Return status from attack/receive function

--- src/modules/Player.js
+++ src/modules/Player.js
@@ -12,19 +12,19 @@ class Player {
   }
 
   attack(x, y) {
-    this.enemy.receive(x, y);
+    return this.enemy.receive(x, y);
   }
 
   compAttack() {
     if (this.id !== "comp") return;
     let legalMoves = this.enemy.myBoard.legalMoves;
     let [x, y] = legalMoves[Math.floor(Math.random() * legalMoves.length)];
-    this.enemy.receive(x, y);
+    return this.enemy.receive(x, y);
   }
 
   receive(x, y) {
     if (this.myBoard.isIllegal(x, y)) return "Illegal Move";
-    this.myBoard.receiveAttack(x, y);
+    return this.myBoard.receiveAttack(x, y);
   }
 }
 
