74400dd1ffa92c39b3e5f2de0c0a02f5b27dae55
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-08-10 22:02:27 +0500

    Add info obj to Ship to save type, coords, axis

--- src/modules/Ship.js
+++ src/modules/Ship.js
@@ -1,18 +1,17 @@
-const Ship = function (id, coords) {
+const Ship = function (info) {
   return {
-    id,
-    coords,
+    info,
     hit: [],
     processHit: function (coord) {
       this.hit.push(coord);
     },
     isSunk: function () {
-      return this.hit.length === this.coords.length ? true : false;
+      return this.hit.length === this.info.length ? true : false;
     },
-    contains: function (arr, obj) {
+    contains: function ([i, j], obj) {
       let compare = false
-      this[obj].forEach((coord) => {
-        if (coord[0] === arr[0] && coord[1] === arr[1]) {
+      this[obj].forEach(([x, y]) => {
+        if (x === i && y === j) {
           compare = true;
           return;
         }
