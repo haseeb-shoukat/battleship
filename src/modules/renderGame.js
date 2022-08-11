bf6fbea2b000cdf5d661559df4697a70f1f592d5
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-08-12 02:38:29 +0500

    Write func to display overlay for ships placement

--- src/modules/renderGame.js
+++ src/modules/renderGame.js
@@ -1,6 +1,7 @@
 const renderGame = (function () {
   const boardOne = document.querySelector(".board-one");
   const boardTwo = document.querySelector(".board-two");
+  const placingBoard = document.querySelector(".placing-board");
 
   for (let x = 0; x < 10; x++) {
     for (let y = 0; y < 10; y++) {
@@ -9,8 +10,46 @@ const renderGame = (function () {
       item.dataset.coord = [x, y].join("");
       boardOne.appendChild(item);
       boardTwo.appendChild(item.cloneNode());
+      placingBoard.appendChild(item.cloneNode());
     }
   }
+
+  const renderPlacingBoard = function (p1) {
+    
+    const ship = { type: "Carrier", len: 5 };
+
+    const boardItems = document.querySelectorAll(
+      ".placing-board > .board-item"
+    );
+
+    boardItems.forEach((item) => {
+      let axis = "y";
+      item.addEventListener("mouseover", (e) => {
+        const [x, y] = item.dataset.coord.split("").map(Number);
+        let coords = [];
+        if (axis === "x") {
+          for (let i = x; i < x + ship.len; i++) {
+            // document
+            //   .querySelector(`.placing-board > [data-coord="${[i, y].join("")}"]`)
+            //   .classList.add("hover");
+            coords.push([i, y]);
+          }
+        } else {
+          for (let i = y; i < y + ship.len; i++) {
+            // document
+            // //   .querySelector(`.placing-board > [data-coord="${[x, i].join("")}"]`)
+            // //   .classList.add("hover");
+            coords.push([x, i]);
+          }
+        }
+      });
+      item.addEventListener("mouseout", (e) => {
+        document
+          .querySelectorAll(".hover")
+          .forEach((item) => item.classList.remove("hover"));
+      });
+    });
+  };
 
   const render = function (p1, p2) {
     renderPlayer(p1);
@@ -42,6 +81,7 @@ const renderGame = (function () {
 
   return {
     render,
+    renderPlacingBoard,
   };
 })();
 
