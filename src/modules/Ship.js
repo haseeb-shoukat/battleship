273d795b8aa87901e6f08fb798c0cd0640aa64de
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-08-11 16:53:42 +0500

    Optimize code/Add info feature for ship type/axis

--- src/modules/Ship.js
+++ src/modules/Ship.js
@@ -1,22 +1,18 @@
 const Ship = function (info) {
   return {
     info,
+    coords: info.coords,
     hit: [],
     processHit: function (coord) {
       this.hit.push(coord);
     },
     isSunk: function () {
-      return this.hit.length === this.info.length ? true : false;
+      return this.hit.length === this.coords.length ? true : false;
     },
-    contains: function ([i, j], obj) {
-      let compare = false
-      this[obj].forEach(([x, y]) => {
-        if (x === i && y === j) {
-          compare = true;
-          return;
-        }
-      });
-      return compare
+    contains: function (coord, obj) {
+      return this[obj].some(
+        (item) => JSON.stringify(item) === JSON.stringify(coord)
+      );
     },
   };
 };
