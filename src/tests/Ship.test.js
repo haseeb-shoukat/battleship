0bda4aff40fca4f5c03ca0e2c4b63e7797c359f4
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-08-11 16:54:08 +0500

    Fix tests after updating Ship factory

--- src/tests/Ship.test.js
+++ src/tests/Ship.test.js
@@ -1,25 +1,31 @@
 import { Ship } from "../modules/Ship";
 
 test("Adds coordinates to hit", () => {
-  const ship = Ship(0, [
+  const ship = Ship({coords: [
     [0, 1],
     [0, 2],
     [0, 3],
-  ]);
+  ]});
   
   ship.processHit([0, 1]);
   expect(ship.contains([0, 1], "hit")).toBe(true);
 });
 
 test("Reports if ship is sunk", () => {
-  const ship = Ship(0, [[0, 1]]);
+  const ship = Ship({coords: [
+    [0, 1],
+  ]});
 
   ship.processHit([0, 1]);
   expect(ship.isSunk()).toBe(true);
 });
 
 test("Reports if ship is NOT sunk", () => {
-  const ship = Ship(0, [[0, 1], [0, 2]]);
+  const ship = Ship({coords: [
+    [0, 1],
+    [0, 2],
+    [0, 3],
+  ]});
 
   ship.processHit([0, 1]);
   expect(ship.isSunk()).toBe(false);
